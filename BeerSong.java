7024c2302ca84c041414475901015769b4c2457b
Ryan Evans <raevans2@oakland.edu>
2016-02-03 23:40:43 -0500

    Changed headers slightly

--- BeerSong.java
+++ BeerSong.java
@@ -1,7 +1,7 @@
 public class BeerSong{
 
 	public static void Ninety_Nine_Bottles_of_Beer(){
-            System.out.println("***************	Program	output	*************************************");
+            System.out.println("*********************	Program	output	*************************************");
             for(int i = 99; i != -1; i--){
 	    	if( i > 1 ){
 		System.out.println(i + " bottles of beer on the wall, " + i +" bottles of beer"); 
@@ -10,7 +10,7 @@ public class BeerSong{
                        System.out.println("1 bottle of beer on the wall, 1 bottle of beer");     
                         }if(i == 0) {
                         	System.out.println( "take one down, pass it around, no more bottles of beer on the wall.");     
-                                System.out.println("*************** End program output *************************************");
+                                System.out.println("********************** End program output *************************************");
               }
            }
 	}
