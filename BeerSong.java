aeedd8855a82ceea0508c7cbb1bd469f8b91d541
Ryan Evans <raevans2@oakland.edu>
2016-02-03 23:34:03 -0500

    Added BeerSong class

--- BeerSong.java
+++ BeerSong.java
@@ -1,16 +1,16 @@
 public class BeerSong{
 
 	public static void Ninety_Nine_Bottles_of_Beer(){
-            System.out.println("***************	Program	output	*************************************");
+            
             for(int i = 99; i != -1; i--){
-	    	if( i > 1 ){
+	    	if( i != 1 ){
 		System.out.println(i + " bottles of beer on the wall, " + i +" bottles of beer"); 
                     }
                      if( i == 1){
                        System.out.println("1 bottle of beer on the wall, 1 bottle of beer");     
                         }if(i == 0) {
                         	System.out.println( "take one down, pass it around, no more bottles of beer on the wall.");     
-                                System.out.println("*************** End program output *************************************");
+                                
               }
            }
 	}
